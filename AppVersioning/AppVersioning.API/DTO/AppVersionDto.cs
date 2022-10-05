16754353dc9e1cac096ebef5c8e65ee88215c974
Alberto Linares <alinares94@protonmail.com>
2022-10-05 21:09:04 +0200

    first commit

---
+++ AppVersioning/AppVersioning.API/DTO/AppVersionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AppVersioning.API.DTO;
+
+public class AppVersionDto
+{
+    public const string SETTINGS_KEY = "AppMinVersions";
+
+    public string Key { get; set; } = string.Empty;
+    public string MinVersion { get; set; } = string.Empty;
+}
