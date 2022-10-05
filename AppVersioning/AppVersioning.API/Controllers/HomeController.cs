a415d7c7a017f3510d6cd9beb5761d904ee489f4
Alberto Linares <alinares94@protonmail.com>
2022-10-05 22:18:12 +0200

    publish config

---
+++ AppVersioning/AppVersioning.API/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppVersioning.API.Controllers;
+[ApiController]
+[Route("")]
+public class HomeController : ControllerBase
+{
+    public HomeController()
+    {
+    }
+
+    [HttpGet]
+    public ActionResult<string> Get()
+    {
+        return Ok("Welcome to App Version Manager!");
+    }
+}
