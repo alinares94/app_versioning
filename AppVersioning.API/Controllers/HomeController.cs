33b7c8ac9e69077a5d3660e06866ab99a64573ab
Alberto Linares <alinares94@protonmail.com>
2022-10-14 18:11:14 +0200

    api version

--- AppVersioning.API/Controllers/HomeController.cs
+++ AppVersioning.API/Controllers/HomeController.cs
@@ -1,17 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace AppVersioning.API.Controllers;
 [ApiController]
 [Route("")]
 public class HomeController : ControllerBase
 {
-    public HomeController()
+    private readonly string _apiVersion;
+    public HomeController(IConfiguration configuration)
     {
+        _apiVersion = configuration.GetSection("ApiVersion").Value;
     }
 
     [HttpGet]
-    public ActionResult<string> Get()
+    public ActionResult<string> Version()
     {
-        return Ok("Welcome to App Version Manager!");
+        var result = $"<h2>Api Version {_apiVersion}</h2>";
+        return Content(result, "text/html");
     }
 }
